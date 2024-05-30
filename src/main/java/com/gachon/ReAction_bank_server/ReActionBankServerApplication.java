97a9786195b85f053eaa0ea53ae8fef61cff0634
han16935 <han1693516@gmail.com>
2024-05-30 20:10:36 +0900

    first commit

---
+++ src/main/java/com/gachon/ReAction_bank_server/ReActionBankServerApplication.java
@@ -0,0 +1,13 @@
+package com.gachon.ReAction_bank_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReActionBankServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReActionBankServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gachon/ReAction_bank_server/ReActionBankServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gachon.ReAction_bank_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReActionBankServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
