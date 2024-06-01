ccdee97c7efb87aff6ef0737a7f1f492213f2d9d
han16935 <han1693516@gmail.com>
2024-06-01 17:07:08 +0900

    Add WebConfig & Interceptor to check unauthorized users & cors setting

---
+++ src/main/java/com/gachon/ReAction_bank_server/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.gachon.ReAction_bank_server.config;
+
+import com.gachon.ReAction_bank_server.interceptor.LoginCheckInterceptor;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login", "/register");
+    }
+}
