5251e834873691d9a3996ed42f414bef20c6b03e
han16935 <han1693516@gmail.com>
2024-06-10 22:59:20 +0900

    application.properties의 ddl-auto none으로 변경

--- src/main/java/com/gachon/ReAction_bank_server/dto/user/controller/LoginRequest.java
+++ src/main/java/com/gachon/ReAction_bank_server/dto/user/controller/LoginRequest.java
@@ -9,26 +9,21 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class LoginRequest {
-
     @NotEmpty(message = "로그인 시 ID는 필수입니다")
     private String userId;
-
     @NotEmpty(message = "로그인 시 비밀번호는 필수입니다")
     private String pw;
-
     @Builder
     private LoginRequest(String userId, String pw) {
         this.userId = userId;
         this.pw = pw;
     }
-
     public static LoginRequest of(String userId, String pw){
         return LoginRequest.builder()
                 .userId(userId)
                 .pw(pw)
                 .build();
     }
-
     public LoginServiceRequest to(){
         return LoginServiceRequest.of(this.userId, this.pw);
     }
